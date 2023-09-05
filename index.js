51b8916f8ce53c99c15746b0c4e8d38c83847585
Amit kumar <ramitgzp56@gmail.com>
2023-09-05 13:06:02 +0530

    nio

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app=require("./app.js")
-app.use(cors({ origin: 'http://127.0.0.1:5173'}));
+app.use(cors);
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 const connectDB=async ()=>{
    await mongoose.connect("mongodb+srv://ramitgzp56:TUQcHFl9w21VGAeU@cluster0.tlwdfrq.mongodb.net/");
 }
