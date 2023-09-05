0eda97705994daddfd659e58c29c16dbd8fea250
Amit kumar <ramitgzp56@gmail.com>
2023-09-05 12:53:54 +0530

    cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app=require("./app.js")
-app.use(cors);
+app.use(cors({ origin: 'http://127.0.0.1:5173'}));
 const connectDB=async ()=>{
    await mongoose.connect("mongodb+srv://ramitgzp56:TUQcHFl9w21VGAeU@cluster0.tlwdfrq.mongodb.net/");
 }
